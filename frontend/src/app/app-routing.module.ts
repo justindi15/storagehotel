cea0f042474102595c70c2c90ea24d1edbbe0cb4
Justin Di <justindi15@yahoo.com>
2019-11-14 23:50:47 -0800

    created checkout views

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { SelectComponent } from './views/select/select.component';
+import { BookComponent } from './views/book/book.component';
+import { PaymentComponent } from './views/payment/payment.component';
+import { ConfirmComponent } from './views/confirm/confirm.component';
 
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent}
+  { path: 'select', component: SelectComponent},
+  { path: 'book', component: BookComponent},
+  { path: 'payment', component: PaymentComponent},
+  { path: 'confirm', component: ConfirmComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -13,12 +13,22 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './register/register.component';
+import { HeaderComponent } from './header/header.component';
+import { SelectComponent } from './views/select/select.component';
+import { BookComponent } from './views/book/book.component';
+import { PaymentComponent } from './views/payment/payment.component';
+import { ConfirmComponent } from './views/confirm/confirm.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    HeaderComponent,
+    SelectComponent,
+    BookComponent,
+    PaymentComponent,
+    ConfirmComponent,
   ],
   imports: [
     BrowserModule,
